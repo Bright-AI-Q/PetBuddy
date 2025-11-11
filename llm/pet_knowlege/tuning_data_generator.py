34f511344cdf401c428489b18b0c5cdeeb209c43
Cindy Surjadi <cindyjosephinesurjadi@gmail.com>
2025-11-11 21:39:19 +0800

    adding breed url lists

--- llm/pet_knowlege/tuning_data_generator.py
+++ llm/pet_knowlege/tuning_data_generator.py
@@ -8,11 +8,134 @@ import os
 # Data is outputed to dog_databse directory
 os.makedirs("dog_database", exist_ok=True)
 
+
 # To populate more websites to scrape here
 urls = ["https://www.dog-breeds.net/affenpinscher/", 
         "https://www.dog-breeds.net/afghan-hound/",
         "https://www.dog-breeds.net/airedale-terrier/",
         ]
+breed_url = {
+    'Affenpinscher':"https://www.dog-breeds.net/affenpinscher/",
+    'Afghan hound': "https://www.dog-breeds.net/afghan-hound/",
+    'African hunting dog': "",
+    'Airedale': "https://www.dog-breeds.net/airedale-terrier/",
+    'American Staffordshire terrier': "https://www.dog-breeds.net/american-Staffordshire-terrier",
+    'Appenzeller': "",
+    'Australian terrier': "https://www.dog-breeds.net/australian-terrier",
+    'Basenji': "https://www.dog-breeds.net/basenji",
+    'Basset': "https://www.dog-breeds.net/basset-hound",
+    'Beagle': "https://www.dog-breeds.net/beagle", 
+    'Bedlington terrier': "https://www.dog-breeds.net/bedlington-terrier",
+    'Bernese mountain dog': "https://www.dog-breeds.net/bernese-mountain-dog",
+    'Black-and-tan Coonhound': "https://www.dog-breeds.net/black-and-tan-coonhound",
+    'Blenheim spaniel': "",
+    'Bloodhound': "https://www.dog-breeds.net/Bloodhound",
+    'Bluetick': "",
+    'Border collie': "https://www.dog-breeds.net/border-collie",
+    'Border terrier': "https://www.dog-breeds.net/border-terrier",
+    'Borzoi': "https://www.dog-breeds.net/Borzoi",
+    'Boston bull': "https://www.dog-breeds.net/boston-terrier",
+    'Bouvier des Flandres': "",
+    'Boxer': "https://www.dog-breeds.net/Boxer",
+    'Brabancon griffon': "",
+    'Briard': "https://www.dog-breeds.net/Briard",
+    'Brittany spaniel': "https://www.dog-breeds.net/Brittany-spaniel",
+    'Bull mastiff': "https://www.dog-breeds.net/Bullmastiff",
+    'Cairn': "https://www.dog-breeds.net/cairn-terrier",
+    'Cardigan': "https://www.dog-breeds.net/cardigan-welsh-corgi",
+    'Chesapeake Bay retriever': "",
+    'Chihuahua': "https://www.dog-breeds.net/Chihuahua",
+    'Chow': "https://www.dog-breeds.net/chow-chow",
+    'Clumber': "https://www.dog-breeds.net/clumber-Spaniel",
+    'Cocker spaniel': "https://www.dog-breeds.net/cocker-Spaniel",
+    'Collie': "https://www.dog-breeds.net/collie",
+    'Curly-coated retriever': "https://www.dog-breeds.net/curly-coated-retriever",
+    'Dandie Dinmont': "https://www.dog-breeds.net/Dandie-dinmont-terrier",
+    'Dhole': "",
+    'Dingo': "", 
+    'Doberman': "https://www.dog-breeds.net/doberman-pinscher",
+    'English foxhound': "https://www.dog-breeds.net/English-foxhound",
+    'English setter': "https://www.dog-breeds.net/English-setter",
+    'English springer': "https://www.dog-breeds.net/English-springer-spaniel",
+    'EntleBucher': "",
+    'Eskimo dog': "",
+    'Flat-coated retriever': "https://www.dog-breeds.net/flat-coated-retriever",
+    'French bulldog': "https://www.dog-breeds.net/french-bulldog",
+    'German shepherd': "https://www.dog-breeds.net/German-shepherd",
+    'German short-haired pointer': "https://www.dog-breeds.net/German-shorthaired-pointer",
+    'Giant schnauzer': "https://www.dog-breeds.net/giant-schnauzer",
+    'Golden retriever': "https://www.dog-breeds.net/golden-retriever",
+    'Gordon setter': "https://www.dog-breeds.net/gordon-setter",
+    'Great Dane': "https://www.dog-breeds.net/great-dane",
+    'Great Pyrenees': "https://www.dog-breeds.net/great-pyrenees",
+    'Greater Swiss Mountain dog': "https://www.dog-breeds.net/greater-swiss-mountain-dog",
+    'Groenendael':"", 
+    'Ibizan hound': "https://www.dog-breeds.net/ibizan-hound",
+    'Irish setter': "https://www.dog-breeds.net/irish-setter/",
+    'Irish terrier': "",
+    'Irish water spaniel': "https://www.dog-breeds.net/irish-water-spaniel",
+    'Irish wolfhound': "https://www.dog-breeds.net/irish-wolfhound",
+    'Italian greyhound': "https://www.dog-breeds.net/italian-greyhound",
+    'Japanese spaniel': "https://www.dog-breeds.net/Japanese-chin",
+    'Keeshond': "https://www.dog-breeds.net/Keeshond",
+    'Kelpie': "",
+    'Kerry blue terrier': "https://www.dog-breeds.net/kerry-blue-terrier",
+    'Komondor': "https://www.dog-breeds.net/Komondor",
+    'Kuvasz': "https://www.dog-breeds.net/Kuvasz",
+    'Labrador retriever': "https://www.dog-breeds.net/labrador-retriever",
+    'Lakeland terrier': "https://www.dog-breeds.net/lakeland-terrier",
+    'Leonberg': "",
+    'Lhasa': "https://www.dog-breeds.net/lhasa-apso",
+    'Malamute': "",
+    'Malinois': "",
+    'Maltese dog': "https://www.dog-breeds.net/Maltese",
+    'Mexican hairless': "",
+    'Miniature pinscher': "https://www.dog-breeds.net/Miniature-pinscher",
+    'Miniature poodle': "",
+    'Miniature schnauzer': "https://www.dog-breeds.net/Miniature-schnauzer",
+    'Newfoundland': "https://www.dog-breeds.net/Newfoundland",
+    'Norfolk terrier': "https://www.dog-breeds.net/Norfolk-terrier",
+    'Norwegian elkhound': "https://www.dog-breeds.net/Norwegian-elkhound",
+    'Norwich terrier': "https://www.dog-breeds.net/Norwich-terrier",
+    'Old English sheepdog': "https://www.dog-breeds.net/old-english-sheepdog",
+    'Otterhound': "https://www.dog-breeds.net/Otterhound",
+    'Papillon': "https://www.dog-breeds.net/Papillon",
+    'Pekinese': "https://www.dog-breeds.net/Pekingese",
+    'Pembroke': "https://www.dog-breeds.net/Pembroke-welsh-corgi",
+    'Pomeranian': "https://www.dog-breeds.net/Pomeranian",
+    'Pug': 
+    'Redbone':
+    'Rhodesian ridgeback':
+    'Rottweiler':
+    'Saint Bernard':
+    'Saluki':
+    'Samoyed':
+    'Schipperke':
+    'Scotch terrier':
+    'Scottish deerhound':
+    'Sealyham terrier':
+    'Shetland sheepdog':
+    'Shih-Tzu':
+    'Siberian husky':
+    'Silky terrier':
+    'Soft-coated wheaten terrier':
+    'Staffordshire bullterrier':
+    'Standard poodle':
+    'Standard schnauzer':
+    'Sussex spaniel':
+    'Tibetan mastiff':
+    'Tibetan terrier':
+    'Toy poodle':
+    'Toy terrier':
+    'Vizsla':
+    'Walker hound':
+    'Weimaraner':
+    'Welsh springer spaniel':
+    'Welsh Highland white terrier':
+    'Whippet':
+    'Wire-haired fox terrier':
+    'Yorkshire terrier':
+}
 headers = {"User-Agent": "Mozilla/5.0"}
 
 def scrape_dog_breed(url):
@@ -98,16 +221,16 @@ def safe_filename(name):
     return name
 
 # --- Run scraper ---
-for url in urls: 
+for breed, url in breed_url: 
     data = scrape_dog_breed(url)
-
-    print(f"🐶 {data['breed']}")
+    data["breed"] = breed
+    print(f"🐶 {breed}")
     for sec in data["sections"][:3]:
         print(f"\n🦴 {sec['section']}")
         print(sec["content"][:300] + "...")
 
     # --- Save to file ---
-    filename = safe_filename(data["breed"]) + ".json"
+    filename = safe_filename(breed) + ".json"
     filepath = os.path.join("dog_database", filename)
 
     with open(filepath, "w", encoding="utf-8") as f:
