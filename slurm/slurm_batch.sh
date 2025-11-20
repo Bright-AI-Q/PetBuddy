17eae58c572fae9de464f9db378dd6d5ac1f02dc
Hwaai Kang Kee <hkee7@gatech.edu>
2025-11-20 21:49:52 +0800

    chore(slurm): spawn multiple jobs using slurm array

--- slurm/slurm_batch.sh
+++ slurm/slurm_batch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 16
+#SBATCH --array=0-5
 #SBATCH --ntasks-per-node=1
 #SBATCH -t 16:00:00
 #SBATCH --gres=gpu:L40S:1
@@ -8,14 +9,16 @@
 #SBATCH -J petbuddy    # jobs name
 #SBATCH -o ./slurm_outs/slurm_%j.out   # file to write logs, prints, etc
 
-cp -r data/pet_cls_training /dev/shm/petbuddy/data/pet_cls_training
+RAM_DIR="/dev/shm/petbuddy/data/pet_cls_training"
+mkdir -p $RAM_DIR
+cp -r data/pet_cls_training/* $RAM_DIR/
 
 if [ $SLURM_GPUS_ON_NODE -gt 1 ]; then
   echo "Running process with $SLURM_GPUS_ON_NODE GPUs"
-  torchrun --standalone --nproc_per_node="gpu" ../tools/experiments/optuna_train.py
+  torchrun --standalone --nproc_per_node="gpu" tools/experiments/optuna_train.py
 else
   echo "Running on single GPU"
-  python -u ../tools/experiments/optuna_train.py
+  python -u tools/experiments/optuna_train.py
 fi
 
 rm -rf /dev/shm/petbuddy/data/pet_cls_training
